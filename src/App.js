497a665f490a177489e6d67ef0a914cfe0082dfd
ShrikantNale17 <85054063+ShrikantNale17@users.noreply.github.com>
2022-05-19 14:42:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// import Add from "./components/Add";
 import List from "./components/List";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Edit from "./components/Edit";
-// import Abc from "./components/abc";
 import Add1 from "./components/Add1";
 import Edit1 from "./components/Edit1";
 
@@ -11,8 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="">
-        {/* <Abc /> */}
-        {/* <List/> */}
         <Routes>
           <Route path="/" element={<List />}></Route>
           <Route path="/add" element={<Add1 />}></Route>
